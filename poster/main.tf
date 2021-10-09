e95da2229dda3dc559cae3eaca3135f899a9f61e
gh-bdr <66869258+gh-bdr@users.noreply.github.com>
2021-10-09 23:49:59 +0100

    add role

--- poster/main.tf
+++ poster/main.tf
@@ -5,3 +5,9 @@ resource "google_service_account" "sa" {
   account_id   = "gke-deployerfromgithub"
   display_name = "deployer"
 }
+
+resource "google_project_iam_member" "role" {
+  project = "multiproject-328509"
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.sa.email}"
+}
